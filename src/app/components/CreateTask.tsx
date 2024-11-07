ade36586a673042f25b71d64cff5719d86e35d1a
anilaloorjoseph <anilaloorjoseph@gmail.com>
2024-11-07 15:45:42 +0200

    Task TM-8 has been completed

--- src/app/components/CreateTask.tsx
+++ src/app/components/CreateTask.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useForm } from "react-hook-form";
 import { useState, FormEvent, ChangeEvent } from "react";
 import { type Task } from "../page";
 
@@ -8,79 +8,88 @@ type createTaskProps = {
 };
 
 export default function CreateTask({ addTaskAction }: createTaskProps) {
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [type, setType] = useState<string>("");
-  const [createdOn, setCreatedOn] = useState<string>("");
-  const [status, setStatus] = useState<string>("");
-
-  const createTask = (e: FormEvent) => {
-    e.preventDefault();
-
-    if (title && description && type && createdOn && status) {
-      const newTask: Task = {
-        title,
-        description,
-        type,
-        createdOn,
-        status,
-      };
-      addTaskAction(newTask);
-    }
-  };
-
-  const handleChange =
-    (set: (value: string) => void) =>
-    (
-      e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
-    ) => {
-      set(e.target.value);
-    };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+      type: "",
+      createdOn: "",
+      status: "",
+    },
+  });
 
   return (
     <div className="container w-2/4 m-auto p-4">
-      <form onSubmit={createTask}>
+      <form
+        onSubmit={handleSubmit((data) => {
+          addTaskAction(data);
+        })}
+      >
         <div className="bg-slate-100 flex flex-col items-center py-4 mt-4">
           <h2 className="text-xl font-medium text-center">Create Task</h2>
           <input
             type="text"
             name="title"
-            required
+            {...register("title", {
+              required: "This is required",
+              maxLength: {
+                value: 30,
+                message: "Maximum length is 30 characters",
+              },
+            })}
             placeholder="Title"
-            value={title}
             className="w-5/6 p-2 my-2 border-zinc-400 border rounded"
-            onChange={handleChange(setTitle)}
           />
+          <small className="block text-center text-red-950">
+            {errors.title?.message}
+          </small>
           <textarea
             name="description"
-            required
+            {...register("description", {
+              required: "This is required",
+              maxLength: 200,
+            })}
             placeholder="Description"
-            value={description}
             className="w-5/6 p-2 my-2 border-zinc-400 border rounded"
-            onChange={handleChange(setDescription)}
           ></textarea>
+          <small className="block text-center text-red-950">
+            {errors.description?.message}
+          </small>
           <input
             type="text"
             name="type"
-            value={type}
+            {...register("type", {
+              required: "This is required",
+              maxLength: 20,
+            })}
             placeholder="Type"
-            required
-            onChange={handleChange(setType)}
             className="w-5/6 p-2 my-2 border-zinc-400 border rounded"
           />
+          <small className="block text-center text-red-950">
+            {errors.type?.message}
+          </small>
           <input
             type="date"
             name="createdOn"
-            value={createdOn}
+            {...register("createdOn", {
+              required: "This is required",
+              maxLength: 30,
+            })}
             placeholder="Created On"
-            required
-            onChange={handleChange(setCreatedOn)}
             className="w-5/6 p-2 my-2 border-zinc-400 border rounded"
           />
+          <small className="block text-center text-red-950">
+            {errors.createdOn?.message}
+          </small>
           <select
             name="status"
-            value={status}
-            onChange={handleChange(setStatus)}
+            {...register("status", {
+              required: "This is required",
+            })}
             className="w-5/6 p-2 my-2 border-zinc-400 border rounded"
           >
             <option value="">--select status--</option>
@@ -88,6 +97,9 @@ export default function CreateTask({ addTaskAction }: createTaskProps) {
             <option value="inprogress">In Progress</option>
             <option value="completed">Completed</option>
           </select>
+          <small className="block text-center text-red-950">
+            {errors.status?.message}
+          </small>
           <button
             type="submit"
             className="bg-slate-500 w-5/6 text-white hover:drop-shadow-xl hover:bg-slate-200  my-2 py-2 rounded"
